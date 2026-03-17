d2eaa42cf97447f7f66db7855ea77daab983c7a4
elcareem <andrewmiracle100@gmail.com>
2026-03-17 16:37:18 +0100

    fix: add base migration to create initial tables for fresh database

--- backend/alembic/versions/5b72bda8d0f3_add_transaction_pin_and_login_count.py
+++ backend/alembic/versions/5b72bda8d0f3_add_transaction_pin_and_login_count.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '5b72bda8d0f3'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = '81e65818b34d'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
