f08513c837f9cbddd9cc137c9805c92e3ab658bb
Akinlade Temitope <akinlade_temitope0506@yahoo.com>
2026-05-15 00:16:11 +0100

    refactor(models): register Ledger models and prune deprecated Admin permissions

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,3 +3,5 @@ from .account import Account
 from .transaction import Transaction
 from .notification import Notification
 from .security_attempt import SecurityAttempt
+from .ledger import LedgerEntry
+from .admin import AdminPermissions, AdminLog
